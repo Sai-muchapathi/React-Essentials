d51120c068184d5b77c97037c02eece1b57189ce
Sai Kiran <saim80191@gmail.com>
2024-01-02 14:26:38 -0500

    Using spread operator and adding tabs.

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,55 @@
 import {CORE_CONCEPTS} from "./data";
 import {CoreConcepts} from "./CoreConcepts";
+
 const descriptors = ['Fundamental', 'Crucial', 'Core'];
 import img from "./assets/config.png";
+
 function randomInt(value) {
     return Math.floor(Math.random() * value);
 }
 
+function Concepts(props) {
+    return (
+        <li>
+            <button>{props.name}</button>
+        </li>
+    );
+}
+
 function App() {
     const descriptor = descriptors[randomInt(2)];
-  return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-            {descriptor} Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-        <main>
-            <section id="core-concepts">
-            <h2>Time to get started!</h2>
-            <ul>
-            <CoreConcepts
-                title={CORE_CONCEPTS[0].title}
-                image={CORE_CONCEPTS[0].image}
-                description={CORE_CONCEPTS[0].description}
-            />
-                <CoreConcepts
-                    title={CORE_CONCEPTS[1].title}
-                    image={CORE_CONCEPTS[1].image}
-                    description={CORE_CONCEPTS[1].description}
-                />
-                <CoreConcepts
-                    title={CORE_CONCEPTS[2].title}
-                    image={CORE_CONCEPTS[2].image}
-                    description={CORE_CONCEPTS[2].description}
-                />
-                <CoreConcepts
-                    title={CORE_CONCEPTS[3].title}
-                    image={CORE_CONCEPTS[3].image}
-                    description={CORE_CONCEPTS[3].description}
-                />
-            </ul>
-            </section>
-        </main>
-    </div>
-  );
+    return (
+        <div>
+            <header>
+                <img src="src/assets/react-core-concepts.png" alt="Stylized atom"/>
+                <h1>React Essentials</h1>
+                <p>
+                    {descriptor} Fundamental React concepts you will need for almost any app you are
+                    going to build!
+                </p>
+            </header>
+            <main>
+                <section id="core-concepts">
+                    <h2>Time to get started!</h2>
+                    <ul>
+                        <CoreConcepts {...CORE_CONCEPTS[0]}/>
+                        <CoreConcepts {...CORE_CONCEPTS[1]}/>
+                        <CoreConcepts {...CORE_CONCEPTS[2]}/>
+                        <CoreConcepts {...CORE_CONCEPTS[3]}/>
+                    </ul>
+                </section>
+                <section id="examples">
+                    <h2>Concepts</h2>
+                    <menu>
+                        <Concepts name={'props'}/>
+                        <Concepts name={'React'}/>
+                        <Concepts name={'props'}/>
+                        <Concepts name={'props'}/>
+                    </menu>
+                </section>
+            </main>
+        </div>
+    );
 }
 
 export default App;
